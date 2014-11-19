de981749b0a6b1a8c826ae21eca2855bfcf3a712
Jamie Patel <jamie@notespublication.com>
2014-11-19 13:47:21 +0000

    To do list works

--- js/dispatcher/AppDispatcher.js
+++ js/dispatcher/AppDispatcher.js
@@ -1,16 +1,20 @@
-var Dispatcher = require('./Dispatcher');
+var Dispatcher = require('flux').Dispatcher;
+var assign = require('object-assign');
 
-var merge = require('react/lib/merge');
+var AppDispatcher = assign(new Dispatcher(), {
 
-var AppDispatcher = merge(Dispatcher.prototype, {
-
-	handleViewAction: function(action) {
-		this.dispatch({
-			source: 'VIEW_ACTION',
-			action: action
-		});
-	};
+  /**
+   * A bridge function between the views and the dispatcher, marking the action
+   * as a view action.  Another variant here could be handleServerAction.
+   * @param  {object} action The data coming from the view.
+   */
+  handleViewAction: function(action) {
+    this.dispatch({
+      source: 'VIEW_ACTION',
+      action: action
+    });
+  }
 
 });
 
-module.exports = AppDispatcher
+module.exports = AppDispatcher;
